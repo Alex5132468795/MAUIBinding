04d2f8947f4ee871f8b5b8552a1a737904e76dc4
ilyaDavidov <ilya.davidov63@mail.ru>
2022-12-20 13:43:50 +0200

    qwe

---
+++ MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿
+using MauiApp5.Model;
+
+namespace MauiApp5;
+
+public partial class MainPage : ContentPage
+{
+	public List<Memori> LL { get; set; }
+	public Memori MM { get; set; }
+ 
+
+    public MainPage()
+	{
+		LL = new List<Memori>();
+        Memori WW = new Memori();
+        Memori EE = new Memori();
+        Memori RR = new Memori();
+        MM = new Memori();
+		WW.fami = "QWQ";
+		WW.name = "OWO";
+		WW.secondName = "OUO";
+		EE.fami = "YIY";
+		EE.name = "UIU";
+		EE.secondName = "OIO";
+		RR.fami = "aweq";
+		RR.name = "ASD";
+		RR.secondName = "ZXC";
+        InitializeComponent();
+        LL.Add(WW);
+		LL.Add(EE);
+		LL.Add(RR);
+		
+		LL.Add(MM);
+	
+		
+        BindingContext = this;
+		
+	
+	}
+
+	public void Picker_SelectedIndexChanged(object sender, EventArgs e)
+	{
+		Fami.SetBinding(Entry.TextProperty, "MM.Fami");
+        labelFami.SetBinding(Label.TextProperty, "MM.Fami");
+		Name.SetBinding(Entry.TextProperty, "MM.Name");
+		labelName.SetBinding(Label.TextProperty, "MM.Name");
+		Second.SetBinding(Entry.TextProperty, "MM.SecondName");
+		labelSecond.SetBinding(Label.TextProperty, "MM.SecondName");
+		Source.SetBinding(Label.TextProperty, "MM.Sour");
+		ImSource.SetBinding(Image.SourceProperty, "MM.Sour");
+    }
+}
+
---
+++ Model/Memori.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp5.Model
+{
+    public class Memori : INotifyPropertyChanged
+    {
+
+        //public string text;
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+        //public string Text
+        //{
+        //    get
+        //    {
+        //        return text;
+        //    }
+        //    set{
+        //        text = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
+        //protected void OnPropertyChanged([CallerMemberName] string tt=null)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(tt));
+        //}
+        //public string this[int index]
+        //{
+        //    get { return this[index]; }
+
+        //}
+        //--------------------------------------------------------------------------
+        public string name;
+        public string secondName;
+        public string fami;
+        public string sour; 
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+        public string SecondName
+        {
+            get
+            {
+                return secondName;
+            }
+            set
+            {
+                secondName = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Fami
+        {
+            get
+            {
+                return fami;
+            }
+            set
+            {
+                fami = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Sour
+        {
+            get
+            {
+                return sour;
+            }
+            set
+            {
+                sour = value;
+                OnPropertyChanged();
+            }
+        }
+        protected void OnPropertyChanged([CallerMemberName] string tt = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(tt));
+        }
+        public string this[int index]
+        {
+            get { return this[index]; }
+
+        }
+    }
+}
